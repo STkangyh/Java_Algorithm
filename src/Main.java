f42f99b53807ae81642f99d0bbe1d43ddb3f4590
STkangyh <k22101149@g.seoultech.ac.kr>
2024-06-25 15:55:25 +0900

    1676

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] S = new int[305];
-        int t = 0;
-        for (int i = 1; i <= n; i++) {
-            S[i] = Integer.parseInt(br.readLine());
-            t += S[i];
+        int answer = 0;
+        while (n >= 5) {
+            answer += n / 5;
+            n /= 5;
         }
-        // Hello World
-        int[] dp = new int[305]; // i번째 계단까지 밟지 않을 계단의 합의 최솟값
-        if (n > 2){
-            dp[1] = S[1];
-            dp[2] = S[2];
-            dp[3] = S[3];
-            for (int i = 4; i < n; i++) {
-                dp[i] = S[i] + Math.min(dp[i - 2], dp[i - 3]);
-            }
-            System.out.println(t - Math.min(dp[n-1], dp[n-2]));
-        } else {
-            System.out.println(t);
-        }
+        System.out.println(answer);
     }
 }
